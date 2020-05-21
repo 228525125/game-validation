41e246545d09cdbb415118491b8a9f4b71c3bb60
chenxian <228525125@users.noreply.github.com>
2020-05-21 19:10:41 +0800

    修改

--- src/main/java/org/cx/game/validator/exception/ValidatorException.java
+++ src/main/java/org/cx/game/validator/exception/ValidatorException.java
@@ -4,12 +4,12 @@ import org.cx.game.validator.tools.I18n;
 
 public class ValidatorException extends RuntimeException {
 
-	public ValidatorException() {
+	public ValidatorException(String playNo) {
 		// TODO Auto-generated constructor stub
 		super(I18n.getMessage("org.cx.game.validator.exception.ValidatorException"));
 	}
 	
-	public ValidatorException(String description) {
+	public ValidatorException(String playNo, String description) {
 		// TODO Auto-generated constructor stub
 		super(I18n.getMessage(description));
 	}
